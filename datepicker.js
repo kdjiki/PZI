e98f28528510e024cbc8178a2b4bae3b75878427
kdjiki <kdjiki00@fesb.hr>
2025-01-19 21:19:51 +0100

    Initial commit

---
+++ datepicker.js
@@ -0,0 +1,132 @@
+export function calendarFunctions(){
+
+const datepicker = document.querySelector(".datepicker");
+const dates = datepicker.querySelector(".dates");
+
+datepicker.hidden=true;
+
+let selectedDate = new Date();
+let year = selectedDate.getFullYear();
+let month = selectedDate.getMonth();
+
+document.querySelector(".date-input").addEventListener("click", () => {
+  datepicker.hidden = !datepicker.hidden;
+});
+
+datepicker.querySelector(".cancel").addEventListener("click", () => {
+  datepicker.hidden = true;
+});
+
+datepicker.querySelector(".apply").addEventListener("click", () => {
+    document.querySelector(".date-input").value = selectedDate.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+  });
+  datepicker.hidden = true;
+});
+
+const nextButton = datepicker.querySelector(".next");
+nextButton.addEventListener("click", () => {
+  if (month === 11) year++;
+  month = (month + 1) % 12;
+  displayDates();
+});
+
+const prevBotton = datepicker.querySelector(".prev");
+prevBotton.addEventListener("click", () => {
+  if (month === 0) year--;
+  month = (month - 1 + 12) % 12;
+  displayDates();
+});
+
+const inputedMonth = datepicker.querySelector(".month-input");
+inputedMonth.addEventListener("change", () => {
+  month = inputedMonth.selectedIndex;
+  displayDates();
+});
+
+const inputedYear = datepicker.querySelector(".year-input");
+inputedYear.addEventListener("change", () => {
+  year = inputedYear.value;
+  displayDates();
+});
+
+
+const handleDateClick = (e) => {
+  const button = e.target;
+
+  // remove the 'selected' class from other buttons
+  const selected = dates.querySelector(".selected");
+  selected && selected.classList.remove("selected");
+
+  // add the 'selected' class to current button
+  button.classList.add("selected");
+
+  // set the selected date
+  selectedDate = new Date(year, month, parseInt(button.textContent));
+};
+
+// render the dates in the calendar interface
+const displayDates = () => {
+  // update year & month whenever the dates are updated
+    inputedMonth.selectedIndex = month;
+    inputedYear.value = year;
+    dates.innerHTML = "";
+
+  //* display the last week of previous month
+
+  // get the last date of previous month
+  const daysOfThePreviusMOnth = new Date(year, month, 0);
+
+  for (let i = 0; i <= daysOfThePreviusMOnth.getDay(); i++) {
+    const text = daysOfThePreviusMOnth.getDate() - daysOfThePreviusMOnth.getDay() + i;
+    const button = createButton(text, true, -1);
+    dates.appendChild(button);
+  }
+
+  //* display the current month
+
+  // get the last date of the month
+  const lastOfMOnth = new Date(year, month + 1, 0);
+
+  for (let i = 1; i <= lastOfMOnth.getDate(); i++) {
+    const button = createButton(i, false);
+    button.addEventListener("click", handleDateClick);
+    dates.appendChild(button);
+  }
+
+  //* display the first week of next month
+
+  const daysOfTheNextMonth = new Date(year, month + 1, 1);
+
+  for (let i = daysOfTheNextMonth.getDay(); i < 7; i++) {
+    const text = daysOfTheNextMonth.getDate() - daysOfTheNextMonth.getDay() + i;
+    const button = createButton(text, true, 1);
+    dates.appendChild(button);
+  }
+};
+
+const createButton = (text, isDisabled = false, type = 0) => {
+  const currentDate = new Date();
+
+  // determine the date to compare based on the button type
+  let comparisonDate = new Date(year, month + type, text);
+
+  // check if the current button is selected
+  const selected = selectedDate.getTime() === comparisonDate.getTime();
+
+  const button = document.createElement("button");
+  button.textContent = text;
+  button.disabled = isDisabled;
+  button.classList.toggle("today",  currentDate.getDate() === text &&
+    currentDate.getFullYear() === year &&
+    currentDate.getMonth() === month && 
+    !isDisabled);
+  button.classList.toggle("selected", selected);
+  return button;
+};
+
+displayDates();
+
+}
---
+++ script.js
@@ -0,0 +1,209 @@
+import calendarFunctions from 'datepicker.js';
+
+//Funkcija za dodavanje događaja
+const addNewCardButton = document.getElementById('add-event');
+
+addNewCardButton.addEventListener('click', handleAddCardButtonClick);
+
+function handleAddCardButtonClick() {
+	const headerOfArticle = prompt('Unesi naziv događaja:', 'Događaj');
+	if (!headerOfArticle) return;
+
+	const imageLocation = prompt('Unesi putanju do slike: ', 'images/fesb-logo.png');
+	if (!imageLocation) return;
+
+	const description = prompt('Unesi opis događaja: ', 'tradicionalni');
+	if (!description) return;
+
+	const startDate = prompt("Unesi datum početka:", "19.1.2025.");
+	const endDate = prompt("Unesi datum završetka:", "19.1.2025.");
+  
+	const newEvent = {
+	  header: headerOfArticle,
+	  image: imageLocation,
+	  description: description,
+	  location: "",
+	  startDate: startDate,
+	  endDate: endDate,
+	};
+
+	const happenings = JSON.parse(localStorage.getItem("events")) || [];
+	
+	// happenings su parsirani eventi (glupo ime zbog razlike)
+	happenings.push(newEvent);
+	
+	// azuriranje i prikaz dogadaja koji su trenutno spremljenu
+	localStorage.setItem("events", JSON.stringify(happenings));
+	displaySavedEvents();
+}
+
+function displaySavedEvents(nameOfTheCity) {
+	const cardContainer = document.getElementById("cards-container");
+	cardContainer.innerHTML = "";
+
+	var events = JSON.parse(localStorage.getItem("events")) || [];
+	if(nameOfTheCity!=undefined & nameOfTheCity!=""){
+		events = events.filter(event => event.location === nameOfTheCity);
+	}else{
+		events = JSON.parse(localStorage.getItem("events")) || [];
+	}
+	
+	events.forEach((event, index) => {
+	  const article = document.createElement("article");
+	  article.classList.add("card");
+  
+	  article.innerHTML = `
+		<div class="card-main">
+		  <div class="card-header">
+			<h2>${event.header}</h2>
+			<img src="${event.image}" alt="Event Image" />
+		  </div>
+		  <div class="card-text-container">
+			<p id="description">Opis događaja: ${event.description}</p>
+			
+			<div id="location-container-${index}">
+				<label for="location-select-${index}">Odaberi lokaciju događaja:</label>
+				<br>
+				<select class="location-select-js" id="location-select-${index}">
+					<option value="">Izaberi lokaciju</option>
+				</select>
+				<button class="confirm-location" data-index="${index}">Potvrdi</button>
+			</div>
+			<p id="location">Lokacija: ${event.location || "Lokacija nije odabrana"}</p>
+			
+
+			 <div class="datepicker-container">
+				<input type="text" class="date-input" placeholder="Izaberi datum" />
+		  
+				<div class="datepicker">
+				  <div class="datepicker-toolbar">
+					<button class="prev">Prošli</button>
+		  
+					<div>
+					  <select class="month-input">
+						<option>Siječanj</option>
+						<option>Veljača</option>
+						<option>Ožujak</option>
+						<option>Travanj</option>
+						<option>Svibanj</option>
+						<option>Lipanj</option>
+						<option>Srpanj</option>
+						<option>Kolovoz</option>
+						<option>Rujan</option>
+						<option>Listopad</option>
+						<option>Studeni</option>
+						<option>Prosinac</option>
+					  </select>
+					  <input type="number" class="year-input" />
+					</div>
+		  
+					<button class="next">Idući</button>
+				  </div>
+				
+				  <div class="days">
+					<span>PON</span>
+					<span>UTO</span>
+					<span>SRI</span>
+					<span>ČET</span>
+					<span>PET</span>
+					<span>SUB</span>
+					<span>NED</span>
+				  </div>
+	
+				  <div class="dates"></div>
+				  <div class="datepicker-end-button">
+					<button class="cancel">Odustani</button>
+					<button class="apply">Primjeni</button>
+				  </div>
+				</div>
+			  </div>
+
+
+
+			<p id="start-date">Datum početka: ${event.startDate}</p>
+			<p id="end-date">Datum završetka: ${event.endDate}</p>
+		  </div>
+		</div>
+		<div class="delete-class">
+		  <button class="delete-event">Obriši događaj</button>
+		</div>
+	  `;
+
+		  article.querySelector(".delete-event").addEventListener("click", () => {
+			let events = JSON.parse(localStorage.getItem("events")) || [];
+	  
+			// trazi za dodadaj koji je trigeran
+			events = events.filter((temp) => temp.header !== event.header);
+			localStorage.setItem("events", JSON.stringify(events));
+		  
+			displaySavedEvents();
+		});
+
+
+	  //FETCHANJE
+	  fetch("https://raw.githubusercontent.com/samayo/country-json/refs/heads/master/src/country-by-capital-city.json")
+	  .then(response => response.json())
+	  .then(data => {
+		  const select = document.getElementById(`location-select-${index}`);
+		  data.forEach((entry) => {
+			  const option = document.createElement("option");
+			  option.value = entry.city ? `${entry.city}, ${entry.country}` : entry.country;
+			  option.textContent = entry.city ? `${entry.city}, ${entry.country}` : entry.country;
+			  select.appendChild(option);
+		  });
+	  })
+	  .catch(error => console.log("Unable to fetch cities", error));
+  
+
+	article.querySelector(".confirm-location").addEventListener("click", () => {
+		const select = document.getElementById(`location-select-${index}`);
+		const selectedLocation = select.value;
+
+		const events = JSON.parse(localStorage.getItem("events")) || [];
+		events[index].location = selectedLocation;
+		localStorage.setItem("events", JSON.stringify(events));
+		displaySavedEvents();
+	});
+
+	  cardContainer.appendChild(article);
+	});
+}
+  
+  document.addEventListener('DOMContentLoaded', async function () {
+	try {
+		displaySavedEvents();
+	}catch (e) {
+		console.error(e);
+	}
+  });
+
+
+
+  fetch("https://raw.githubusercontent.com/samayo/country-json/refs/heads/master/src/country-by-capital-city.json")
+  .then(response => response.json())
+  .then(data => {
+	  const select = document.getElementById(`location-select`);
+	  data.forEach((entry) => {
+		  const option = document.createElement("option");
+		  option.value = entry.city ? `${entry.city}, ${entry.country}` : entry.country;
+		  option.textContent = entry.city ? `${entry.city}, ${entry.country}` : entry.country;
+		  select.appendChild(option);
+	  });
+  })
+  .catch(error => console.log("Unable to fetch cities", error));
+
+
+  
+function getCityInFilter(){
+	const buttonFound = document.querySelector(".found-events-on-location");
+
+	buttonFound.addEventListener("click", () => {
+	  const select = document.getElementById("location-select");
+	select.value != undefined ? displaySavedEvents(select.value) : select.value = undefined;
+	
+});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	getCityInFilter();
+  });
